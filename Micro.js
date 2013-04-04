94ed61e03235344a3e178c21060a98f40cc0041f
Max <max@motovilov.com>
2013-04-04 11:49:23 -0500

    Micro 0.3 -- uses separate holder object for callback

--- Micro.js
+++ Micro.js
@@ -6,19 +6,16 @@
 
 	// Based on Max' micro-deferred: https://gist.github.com/MaxMotovilov/4750596
 
-	function Micro(val, cb){
-		if(val instanceof Micro){
-			// special case: a dependent promise
-			ice.assert(typeof cb == "function", "Attempt to improperly construct a dependent promise");
-			this.parent = val;
-			this.parentChainIndex = this.parent.chain.length;
-			this.parent.chain.push(cb);
-		}else if(arguments.length){
-			// special case: a resolved promise
-			this.value = val;
-			return;
+	function Micro(val) {
+		ice.assert( !(val instanceof Micro), "Attempt to improperly construct a dependent promise" );
+
+		if(val instanceof Callback) {
+			this.parent = val;			// State W: Weak promise
+		} else if( arguments.length ) {
+			this.value = val;			// State R: Resolved promise
+		} else {
+			this.chain = [];			// State P: Regular promise
 		}
-		this.chain = [];
 	}
 
 	Micro.prototype = {
@@ -28,55 +25,73 @@
 			return val instanceof Micro && (val.done(this.resolve.bind(this)) || true);
 		},
 
-		resolve: function(val, isEvent){
-			ice.assert(!("value" in this), "Attempt to resolve an already resolved promise.");
-			ice.assert(!("parent" in this), "Attempt to directly resolve a dependent promise");
-			ice.assert(this.chain, "BUG: malformed promise object");
+		resolve: function(val, isEvent) {
+			// ASSERT( state == P )
+			ice.assert(!("value" in this) && !("parent" in this) && this.chain, "Promise cannot be resolved" );
 
-			if(isEvent || !this.rebind(val)){
-				for(var i = 0; i < this.chain.length; ++i){
-					this.chain[i](val, isEvent);
-				}
-				if(!isEvent){
+			if( isEvent || !this.rebind(val) ) {
+				for(var i=0; i<this.chain.length; ++i)
+					this.chain[i].resolve( val, isEvent );
+					
+				if(!isEvent) {
+					// state: P -> R
 					this.value = val;
 					delete this.chain;
 				}
 			}
 		},
 
-		then: function(cb){
-			if("value" in this){
+		then: function(cb) {
+			if( "value" in this || !this.addCallback(cb) ) {
 				var value = cb(this.value);
 				return value instanceof Micro ? value : new Micro(value);
+			} else {
+				return new Micro( this.chain[this.chain.length-1] );
 			}
-			this.connect();
-			return new Micro(this, cb);
 		},
 
-		done: function(cb){
-			if("value" in this){
+		done: function(cb) {
+			if( "value" in this || !this.addCallback(cb) )
 				cb(this.value);
-			}else{
-				this.connect();
-				this.chain.push(cb);
-			}
 		},
 
-		connect: function(){
-			if(this.parent){
-				this.parent.chain[this.parentChainIndex] =
-					callAndForward(this.parent.chain[this.parentChainIndex], this);
+		addCallback: function(cb) {
+			if( "parent" in this ) {
+				var parent = this.parent;
 				delete this.parent;
-				delete this.parentChainIndex;
+
+				if( "value" in parent ) {
+					// state: W -> R
+					this.value = parent.value;
+					return false;
+				}
+
+				// state: W -> P
+				parent.promise = this;
+				this.chain = [];
+			}
+
+			this.chain.push( new Callback(cb) );
+			return true;
+		}
+	};
+
+	function Callback(cb) {
+		this.callback = cb;
+	}
+
+	Callback.prototype = {
+		resolve: function(val, isEvent) {
+			var value = this.callback(val);
+			if( this.promise ) {
+				this.promise.resolve( value, isEvent );
+				if( !isEvent ) 
+					delete this.promise;
+			} else if( !isEvent ) {
+				this.value = value;
 			}
 		}
 	};
 
-	return Micro;
-
-	function callAndForward(cb, promise){
-		return function(val, isEvent){
-			promise.resolve(cb(val), isEvent);
-		};
-	}
+	return Micro;	
 });
