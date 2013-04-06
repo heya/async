651c148f51b42d7326781ee31e4b344c6daea135
Max <max@motovilov.com>
2013-04-05 22:56:15 -0500

    2nd argument for Micro's callback: false if end of chain

--- Micro.js
+++ Micro.js
@@ -108,7 +108,7 @@
 
 	Callback.prototype = {
 		resolve: function(val, isEvent){
-			val = this.callback(val);
+			val = this.callback(val,!!this.promise);
 			delete this.parent;
 			if(this.promise){
 				this.promise.resolve(val, isEvent);
