de8494d728e1a7c232e1ee7a6bd735af8f05c3be
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2016-02-29 10:36:07 -0600

    Appending algorithms to relevant deferreds.

---
+++ FastDeferred-ext.js
@@ -0,0 +1,15 @@
+/* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
+(["./FastDeferred", "./generic/compositions"], function(Deferred, compositions){
+	"use strict";
+
+	var algos = compositions(Deferred);
+
+	Deferred.all = algos.all;
+	Deferred.par = algos.par;
+	Deferred.any = algos.any;
+	Deferred.one = algos.one;
+	Deferred.race = algos.race;
+	Deferred.when = algos.when;
+
+	return Deferred;
+});
