82290b5c6a1324e9c379267706276c384213ab6f
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2016-02-29 10:35:46 -0600

    Promise-based algorithms.

---
+++ std.js
@@ -0,0 +1,6 @@
+/* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
+(["./generic/compositions"], function(compositions){
+	"use strict";
+
+	return compositions();
+});
