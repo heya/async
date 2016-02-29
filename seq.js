e27778edb5e30c499ad0172b358391c16f50ab41
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2016-02-29 10:35:24 -0600

    Reimplemented top-level modules.

--- seq.js
+++ seq.js
@@ -1,6 +1,6 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
-(["./Deferred", "./generic/whenDone"], function(Deferred, instrumentWhenDone){
+(["./Deferred", "./generic/seq"], function(Deferred, instrumentSeq){
 	"use strict";
 
-	return instrumentWhenDone(Deferred);
+	return instrumentSeq(Deferred);
 });
--- when.js
+++ when.js
@@ -1,15 +1,6 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
-(["./Deferred"], function(Deferred){
+(["./Deferred", "./generic/when"], function(Deferred, instrumentWhen){
 	"use strict";
 
-	return function when(value, callback, errback, progback){
-		var deferred;
-		if(value instanceof Deferred.Promise){
-			deferred = value;
-		}else{
-			deferred = new Deferred();
-			deferred.resolve(value);
-		}
-		return arguments.length > 1 ? deferred.then(callback, errback, progback) : deferred;
-	};
+	return instrumentWhen(Deferred);
 });
