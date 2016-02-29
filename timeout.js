5ff11abe061cd08d6198e78edffc5d7b103971f1
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2016-02-29 16:31:32 -0600

    Made timeout() more readable, and supporting "uncaught".

--- timeout.js
+++ timeout.js
@@ -10,15 +10,22 @@
 	}
 
 	function actOn(P, verb, DefaultResolveTo){
-		return function(ms, resolveTo){
+		return function(ms, uncaught, resolveTo){
+			if(verb === "resolve"){
+				resolveTo = uncaught;
+				uncaught = void 0;
+			}
+			var value = ms;
+			if(typeof resolveTo != "undefined"){
+				value = resolveTo;
+			}else if(DefaultResolveTo){
+				value = new DefaultResolveTo(ms);
+			}
 			return new P(function(resolve, reject, cancel){
 				var handle = setTimeout(function(){
 					if(handle){
 						handle = null;
-						(verb === "resolve" ? resolve : reject)(
-							typeof resolveTo !== "undefined"
-							? resolveTo : DefaultResolveTo
-							? new DefaultResolveTo(ms) : ms);
+						(verb === "resolve" ? resolve : reject)(value, uncaught);
 					}
 				}, Math.max(ms, 0));
 				typeof cancel == "function" && cancel(function(){
