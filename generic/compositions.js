56f488ed520b55b8b9bf078312a9ee3ee6eed8ef
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2016-02-29 18:26:57 -0600

    No need for when() dependency.

--- generic/compositions.js
+++ generic/compositions.js
@@ -1,5 +1,5 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
-(["../when"], function(when){
+([], function(){
 	"use strict";
 
 	// based on https://github.com/MaxMotovilov/node-promise/blob/master/promise.js
@@ -104,16 +104,17 @@
 	};
 
 	function instrument(Type, flag, Deferred){
+		var P = Deferred && Deferred.Wrapper || Promise;
+
 		return function(array){
 			array = Array.prototype.slice.call(array instanceof Array ? array : arguments, 0);
 
-			var type = new Type(array, flag),
-				P = Deferred && Deferred.Wrapper || Promise;
+			var type = new Type(array, flag);
 			return new P(function(resolve, reject, cancel){
 				if(type.todo){
 					array.forEach(function(p, i){
 						if(p && typeof p.then == "function"){
-							when(p, Deferred).then(type.succeed(i, resolve, reject), type.failed(i, resolve, reject));
+							p.then(type.succeed(i, resolve, reject), type.failed(i, resolve, reject));
 						}
 					});
 				}else{
@@ -125,7 +126,7 @@
 	}
 
 	return function(Deferred){
-		var race = instrument(Any, true,  Deferred);
+		var race = instrument(Any, true, Deferred);
 		return {
 			all: instrument(All, true,  Deferred),
 			par: instrument(All, false, Deferred),
