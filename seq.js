52939ab12ffaf731c87b38ae6b8813ffe7d6fcea
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2016-03-17 22:18:50 -0500

    Moved seq() to the root.

--- seq.js
+++ seq.js
@@ -1,6 +1,14 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
-(["./Deferred", "./generic/seq"], function(Deferred, instrumentSeq){
+(["./when"], function(when){
 	"use strict";
 
-	return instrumentSeq(Deferred);
+	return function seq(array, Deferred){
+		if(!(array instanceof Array)){
+			array = Array.prototype.slice.call(arguments, 0);
+			Deferred = null;
+		}
+		return function(initial){
+			return array.reduce(function(acc, fn){ return acc.then(fn); }, when(initial, Deferred));
+		};
+	};
 });
